aa6b7a9dab5f9dcaae054ea4e7ef793db021a966
KatalonecSpb <79518241+KatalonecSpb@users.noreply.github.com>
2022-12-31 21:20:50 +0300

    Update Main.java

--- Maven_JDBC_Hibernate_MySQL/src/main/java/jm/task/core/jdbc/Main.java
+++ Maven_JDBC_Hibernate_MySQL/src/main/java/jm/task/core/jdbc/Main.java
@@ -4,8 +4,6 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.*;
-
 public class Main {
 
     private static final UserService userService = new UserServiceImpl();
