4f680d8779d37b1101aaedb420d8852d0980577c
KatalonecSpb <79518241+KatalonecSpb@users.noreply.github.com>
2023-01-04 13:45:06 +0300

    Update Util.java

--- Maven_JDBC_Hibernate_MySQL/src/main/java/jm/task/core/jdbc/util/Util.java
+++ Maven_JDBC_Hibernate_MySQL/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
-    private static final String USERNAME = "rooot";
+    private static final String USERNAME = "root";
     private static final String PASSWORD = "rooot";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
